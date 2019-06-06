3c6ebab8f99e8ddb908336c821d098b220c82f6b
aamnakhan <aamna@getsimpl.com>
2019-06-06 13:01:02 -0700

    updates questions

---
+++ _site/data.js
@@ -0,0 +1,114 @@
+// Questions
+
+
+var q = [
+//product
+'What are you working on?',
+'Who needs your product?',
+'How do you know customers need what you’re making? How do you know people want this?',
+'Why isn’t someone already doing this?',
+'What, exactly, makes you different from existing options?',
+'What do you understand that others don’t?',
+'What have you learned so far from working on your product?',
+'What are your users doing now?',
+'What makes new users try you?',
+'How is your product different?',
+'Why do the reluctant users hold back?',
+'What resistance will users have to trying you and how will you overcome it?',
+'How are you understanding customer needs?',
+'What are the top things your users want?',
+'What has surprised you about user behavior?',
+'What new things have you learnt?',
+'What’s new about what you make?',
+'What problems and hurdles are you anticipating? How will you overcome them?',
+'Six months from now, what’s going to be your biggest problem?',
+'Where is the rocket science here?',
+
+'How will you make money?',
+'How big an opportunity is there?',
+'How much money could you make per year?',
+'How are you meeting customers?',
+'What is your distribution strategy? How will you grow? Where do new users come from? How do users find out about you?',
+'How many users do you have?',
+'How much does customer acquisition cost?',
+'How many people are in your target market?',
+'How many users do your competitors have?',
+'How much are your competitors making?',
+'What are some trends in your target market, and related markets?',
+'Why not work directly with brands instead of designers?',
+'What is your growth like?',
+'What is your user growth rate?',
+'What’s the conversion rate?',
+'Who is going to be your first paying customer?',
+'How many users are paying?',
+'How big is the men\'s apparel market? Kids apparel market?',
+'What does success look like for you?',
+'Who are your competitors?',
+'Who might become competitors?',
+'What competition do you fear most?',
+
+
+'Why will you succeed over others? ',
+'Why did you choose this idea? Why did you pick this idea to work on?',
+'How does your product work in more detail?',
+'What do you understand about your users?',
+'What domain expertise do you have?',
+'What are the key things about your field that outsiders don’t understand?',
+'What’s an impressive thing you have done?',
+'How did your team meet?',
+'Why did your team get together?',
+'Who in your team does what?',
+'Who would you hire or how would you add to your team? Who would be your next hire?',
+'What part of your project are you going to build first? ',
+'What are you going to do next?',
+'What is the next step with the product evolution?',
+'If your startup succeeds, what additional areas might you be able to expand into?',
+
+'How big are your competitors?',
+'What is your burn rate?',
+'Have you raised funding?',
+'How long can you go before funding?',
+'What will you do if we don’t fund you? ',
+'Would you relocate to Silicon Valley during YC?',
+'Who is “the boss”? ',
+'How do we know your team will stick together? Will your team stick at this?',
+'What else have you created together?',
+'Are you open to changing your idea?',
+'Someone just showed us an idea like this right before you guys. I don’t like it. What else do you have?',
+'Why do you want to startup?',
+'Why do you want to do this?',
+'What systems have you hacked?',
+'Tell us about a tough problem you solved?',
+'In what ways are you resourceful?',
+'What is something surprising you have done?',
+'What’s the funniest thing that has happened to you?',
+'What’s the worst thing that has happened? What’s the biggest mistake you have made?',
+'What keeps you up at night?',
+'Why are you uniquely qualified to work on this?',
+'Why do you want to dedicate your life to working on this?',
+'What is your unfair advantage?',
+'What if a competitor comes in with more money?',
+'How much does acquiring a designer cost you?',
+'Do you think there will be multi tenancy with boutiques?',
+'How many designers do you need to service 2500 customers?'
+//biz model
+//user acquisition
+//traction & spends
+//competition
+//market
+//alternative solutions to same problem
+//team
+//hiring, roadmap, what next
+
+];
+
+// Tips
+var tips = [
+	'Try to have quick and concise answers.',
+	'Try to answer in 2 to 3 sentences.',
+	'Answer like you would in a conversation, this is not a presentation.',
+	'When talking to PG and the other YC partners, be substantive, and get straight to the point.',
+	'DO NOT BEAT AROUND THE BUSH. Do not hem and haw. "Uhm"s and "uh"s don&#39;t lend an aura of intelligence.',
+	'Use precise and concise language. You aren&#39;t pitching a VC that will ask you what your "secret sauce" is.',
+	'He talks quickly, and so do the other YC partners. Try to match their tempo (as you should when talking to anyone).'
+];
